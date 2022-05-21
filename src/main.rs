327665b6b6e712657186f13f73326d8feabddb1a
Yasin <yasincaykur@gmail.com>
2022-05-21 23:38:23 +0300

    no space added

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 use std::collections::HashMap;
 
 fn no_space(x : String) -> String{
-    String::new()  
+    x.split(" ").collect::<String>()
 }
 
 fn find_average(slice: &[f64]) -> f64 {
@@ -371,6 +371,15 @@ mod tests {
         ];
         assert_eq!(find_average(&input), 200.0 / 13.0);
         assert_eq!(find_average(&[]), 0.0);
+    }
+
+    #[test]
+    fn test_no_space() {
+      assert_eq!("8j8mBliB8gimjB8B8jlB", no_space("8 j 8   mBliB8g  imjB8B8  jl  B".to_string()));
+      assert_eq!("88Bifk8hB8BB8BBBB888chl8BhBfd", no_space("8 8 Bi fk8h B 8 BB8B B B  B888 c hl8 BhB fd".to_string()));
+      assert_eq!("8aaaaaddddr", no_space("8aaaaa dddd r     ".to_string()));
+      assert_eq!("jfBmgklf8hg88lbe8", no_space("jfBm  gk lf8hg  88lbe8 ".to_string()));
+      assert_eq!("8jaam", no_space("8j aam".to_string()));        
     }
 
 }
