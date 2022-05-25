a1e2195ea0ae825104c67eb22cdc55a68ff5e2a6
Yasin <yasincaykur@gmail.com>
2022-05-25 12:14:57 +0300

    mult table added

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,16 @@
 
 use std::collections::HashMap;
 
+fn multi_table(n: u64) -> String {
+    let mut multi_table_string = String::new();
+    for i in 1..=10 {
+        let m = format!("{} * {} = {}\n", i, n, i*n);
+        multi_table_string.push_str(&m);
+    }
+    multi_table_string.trim_end().to_string()
+
+}
+
 fn bin_to_decimal(inp: &str) -> i32 {
     i32::from_str_radix(inp, 2).unwrap()
 }
@@ -530,6 +540,11 @@ mod tests {
         assert_eq!(bin_to_decimal("0"), 0);
         assert_eq!(bin_to_decimal("1"), 1);
         assert_eq!(bin_to_decimal("1001001"), 73);
+    }
+    #[test]
+    fn test_multi_table() {
+        assert_eq!(multi_table(5), "1 * 5 = 5\n2 * 5 = 10\n3 * 5 = 15\n4 * 5 = 20\n5 * 5 = 25\n6 * 5 = 30\n7 * 5 = 35\n8 * 5 = 40\n9 * 5 = 45\n10 * 5 = 50");
+        assert_eq!(multi_table(1), "1 * 1 = 1\n2 * 1 = 2\n3 * 1 = 3\n4 * 1 = 4\n5 * 1 = 5\n6 * 1 = 6\n7 * 1 = 7\n8 * 1 = 8\n9 * 1 = 9\n10 * 1 = 10");
     }
 
 }
